adfae382772a457c9c9c0e5b3b78e17d2ed7794a
ayfatoye <afatoye@tamu.edu>
2024-03-04 03:34:40 -0600

    finally established a connection from flask to supabase

--- app.py
+++ app.py
@@ -1,24 +1,69 @@
+# from flask import Flask
+# from blueprints.client.client import client_bp
+# # from flask_sqlalchemy import SQLAlchemy
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# app = Flask(__name__)
+
+# supabase_url = os.getenv('SUPABASE_URL')
+# supabase_key = os.getenv('SUPABASE_KEY')
+# database_name = os.getenv('DATABASE_NAME')
+# database_user = os.getenv('DATABASE_USER')
+# database_password = os.getenv('DATABASE_PASSWORD')
+# database_host = os.getenv('DATABASE_HOST')
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{database_user}:{database_password}@{database_host}/{database_name}'
+
+# # db = SQLAlchemy(app)
+
+# app.register_blueprint(client_bp)
+
+# if __name__ == '__main__':
+    # app.run(debug=True)
+
 from flask import Flask
-from blueprints.client.client import client_bp
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+db = SQLAlchemy()
+
 app = Flask(__name__)
 
-supabase_url = os.getenv('SUPABASE_URL')
-supabase_key = os.getenv('SUPABASE_KEY')
-database_name = os.getenv('DATABASE_NAME')
-database_user = os.getenv('DATABASE_USER')
-database_password = os.getenv('DATABASE_PASSWORD')
-database_host = os.getenv('DATABASE_HOST')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{database_user}:{database_password}@{database_host}/{database_name}'
+print("HERE: " + os.getenv('SQLALCHEMY_DATABASE_URI'))
 
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-app.register_blueprint(client_bp)
+db.init_app(app)
 
+class TestTable(db.Model):
+    __tablename__ = 'test_table'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=True)
+
+    def __init__(self, message):
+        self.message = message
+
+# Function to add an entry to test_table
+def add_test_table_entry(message):
+    # Create an instance of TestTable
+    new_entry = TestTable(message=message)
+    
+    # Add the new entry to the session and commit it
+    db.session.add(new_entry)
+    db.session.commit()
+
+# Add this part to run only if this is the main module
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Create tables
+    with app.app_context():
+        db.create_all()
+        add_test_table_entry('Hello, this is a test message!')
+
+
+
--- blueprints/client/client.py
+++ blueprints/client/client.py
@@ -27,7 +27,7 @@ from colorama import Fore, Style
 
 ################################################################################################
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
 from extensions import db  # Import the shared db instance
 from models import Client, HairProfile, ClientInterest, ClientAddress
 
@@ -43,64 +43,63 @@ def client_profile():
 
     print(Fore.GREEN + data.get('fname', "fname not included") + Fore.RESET)
 
-    return {"message": "progress"}
 
     # I'm creating and inserting instances of HairProfile, ClientAddress, and ClientInterest
-#     hair_profile = create_hair_profile(data['hair_profile'])
-#     client_address = create_client_address(data['address'])
-#     db.session.add(hair_profile)
-#     db.session.add(client_address)
-#     db.session.flush()
+    hair_profile = create_hair_profile(data['hair_profile'])
+    client_address = create_client_address(data['address'])
+    db.session.add(hair_profile)
+    db.session.add(client_address)
+    db.session.flush()
 
-#     # Creating and inserting interests
-#     interests_ids = []
-#     for interest in data['interests']:
-#         client_interest = ClientInterest(hair_id=hair_profile.id, interest=interest)
-#         db.session.add(client_interest)
-#         interests_ids.append(client_interest.id)
+    # Creating and inserting interests
+    interests_ids = []
+    for interest in data['interests']:
+        client_interest = ClientInterest(hair_id=hair_profile.id, interest=interest)
+        db.session.add(client_interest)
+        interests_ids.append(client_interest.id)
     
-#     # Type shit. Now I'll create the Client instance with foreign keys from HairProfile and ClientAddress
-#     client = Client(
-#         hair_id=hair_profile.id,
-#         address_id=client_address.id,
-#         fname=data['fname'],
-#         lname=data['lname'],
-#         ethnicity=data['ethnicity'],
-#         stylist_should_know=data['stylist_should_know']
-#     )
-#     db.session.add(client)
+    # Type shit. Now I'll create the Client instance with foreign keys from HairProfile and ClientAddress
+    client = Client(
+        hair_id=hair_profile.id,
+        address_id=client_address.id,
+        fname=data['fname'],
+        lname=data['lname'],
+        ethnicity=data['ethnicity'],
+        stylist_should_know=data['stylist_should_know']
+    )
+    db.session.add(client)
 
-#     try:
-#         db.session.commit()
-#     except Exception as e:
-#         db.session.rollback()
-#         abort(500, description=str(e))
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        abort(500, description=str(e))
 
-#     return jsonify(client_id=client.id, hair_profile_id=hair_profile.id, client_address_id=client_address.id, client_interest_ids=interests_ids), 201
-#     return jsonify({"message": "Client profile data 2"})
+    return jsonify(client_id=client.id, hair_profile_id=hair_profile.id, client_address_id=client_address.id, client_interest_ids=interests_ids), 201
+    return jsonify({"message": "Client profile data 2"})
 
-# def create_hair_profile(hair_data):
-#     hair_profile = HairProfile(
-#         thickness=hair_data['thickness'],
-#         hair_type=hair_data['hair_type'],
-#         hair_gender=hair_data['hair_gender'],
-#         color_level=hair_data['color_level'],
-#         color_hist=hair_data['color_hist']
-#     )
-#     return hair_profile
+def create_hair_profile(hair_data):
+    hair_profile = HairProfile(
+        thickness=hair_data['thickness'],
+        hair_type=hair_data['hair_type'],
+        hair_gender=hair_data['hair_gender'],
+        color_level=hair_data['color_level'],
+        color_hist=hair_data['color_hist']
+    )
+    return hair_profile
 
-# def create_client_address(address_data):
-#     client_address = ClientAddress(
-#         street=address_data['street'],
-#         city=address_data['city'],
-#         state=address_data['state'],
-#         zip_code=address_data['zip_code'],
-#         country=address_data['country'],
-#         comfort_radius=address_data['comfort_radius'],
-#         longitude=address_data['longitude'],
-#         latitude=address_data['latitude']
-#     )
-#     return client_address
+def create_client_address(address_data):
+    client_address = ClientAddress(
+        street=address_data['street'],
+        city=address_data['city'],
+        state=address_data['state'],
+        zip_code=address_data['zip_code'],
+        country=address_data['country'],
+        comfort_radius=address_data['comfort_radius'],
+        longitude=address_data['longitude'],
+        latitude=address_data['latitude']
+    )
+    return client_address
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
