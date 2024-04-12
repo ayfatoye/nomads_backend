2f12d1017a72d3f539698bcd8accc8b6a72a18a9
ayfatoye <afatoye@tamu.edu>
2024-04-12 14:53:31 -0500

    changed app.py to allow incoming requests from not just the local host

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# print("HERE: " + os.getenv('SQLALCHEMY_DATABASE_URI'))
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 db.init_app(app)
@@ -18,13 +16,16 @@ db.init_app(app)
 app.register_blueprint(client_bp, url_prefix='/client')
 app.register_blueprint(stylist_bp, url_prefix='/stylist')
 
-# Add this panrt to run only if this is the main module
+# if __name__ == '__main__':
+#     # Create tables
+#     with app.app_context():
+#         db.create_all()
+#         # add_test_table_entry('Hello, this is a test message!')\
+#         app.run(debug=True)
+
 if __name__ == '__main__':
-    # Create tables
     with app.app_context():
         db.create_all()
-        # add_test_table_entry('Hello, this is a test message!')\
-        app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=10000)
 
 
